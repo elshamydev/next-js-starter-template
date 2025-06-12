18dd4cfb435534b1b1931e343775809f6bb1310e
Ibrahim Elshamy <61933974+elshamydev@users.noreply.github.com>
2025-06-12 17:49:14 +0300

    Add wordpress-theme-qatar-tender/footer.php 1749739755408

---
+++ wordpress-theme-qatar-tender/footer.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> Qatar Tender Management Platform. All rights reserved.</p>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
